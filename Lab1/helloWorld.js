2de6b456844ba2a5edf6a4f9f47b2cf59560414f
Stefano Pogliani <Stefano.Pogliani@fr.ibm.com>
2018-02-15 19:25:04 +0100

    ok

---
+++ Lab1/helloWorld.js
@@ -0,0 +1,65 @@
+/*
+ * © Copyright IBM Corp. 2017
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+ if(typeof(dojo) != "undefined") {
+	require(["dojo/domReady!"], function(){
+        try {
+            //
+            // utility function to let us wait for a specific element of the page to load...
+            //
+            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                if(!elXpathRoot) var elXpathRoot = dojo.body();
+                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                if(!waitTime) var waitTime = 1;  // 1000=1 second
+                if(!elXpath) return;
+                var waitInter = 0;  // current interval
+                var intId = setInterval( function(){
+                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
+
+                    clearInterval(intId);
+                    if( waitInter >= maxInter) { 
+                        console.log("**** WAITFOR [" + elXpath + "] WATCH EXPIRED!!! interval " + waitInter + " (max:" + maxInter + ")");
+                    } else {
+                        console.log("**** WAITFOR [" + elXpath + "] WATCH TRIPPED AT interval " + waitInter + " (max:" + maxInter + ")");
+                        callback();
+                    }
+                }, waitTime);
+            };
+            //
+            // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
+            // before we proceed to customize the page...
+            //
+            waitFor( function(){
+                //
+                // example customization of the "Updates" title...
+                //
+                var updatesDescription = document.getElementById("asDesc");
+                var originalText = updatesDescription.textContent;
+                updatesDescription.textContent = "Hello Customizer: " + originalText;
+                updatesDescription.style = "color:#ff0000";
+                //
+                // ... more of your own customization code here please :-) let's see what you come up with!! ...
+                //
+                dojo.query("span.shareSome-title")[0].textContent = "Hello " + lconn.homepage.userName + " -";
+                //
+                // wait until the "loading..." node has been hidden, indicating that we have loaded content.
+                //
+            }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
+      } catch(e) {
+          alert("Exception occurred in helloWorld: " + e);
+      }
+   });
+}
