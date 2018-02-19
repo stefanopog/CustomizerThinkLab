b169396bb770cc586d8d46bf58eb0209ee0802fa
stefanopog <stefano@poglianis.net>
2018-02-19 19:03:06 +0100

    Update helloWorld.js

--- Lab1/helloWorld.js
+++ Lab1/helloWorld.js
@@ -14,53 +14,43 @@
  * permissions and limitations under the License.
  */
 
- if(typeof(dojo) != "undefined") {
+if (typeof(dojo) != "undefined") {
 	require(["dojo/domReady!"], function(){
         try {
-            //
-            // utility function to let us wait for a specific element of the page to load...
-            //
-            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
-                if(!elXpathRoot) var elXpathRoot = dojo.body();
-                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
-                if(!waitTime) var waitTime = 1;  // 1000=1 second
-                if(!elXpath) return;
-                var waitInter = 0;  // current interval
-                var intId = setInterval( function(){
-                    if( ++waitInter < maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
+          var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                          if (!elXpathRoot) var elXpathRoot = dojo.body();
+                          if (!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                          if (!waitTime) var waitTime = 1;  // 1000=1 second
+                          if (!elXpath) return;
+                          var waitInter = 0;  // current interval
+                          var intId = setInterval( function(){
+                              if ( ++waitInter < maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
 
-                    clearInterval(intId);
-                    if( waitInter >= maxInter) { 
-                        console.log("**** WAITFOR [" + elXpath + "] WATCH EXPIRED!!! interval " + waitInter + " (max:" + maxInter + ")");
-                    } else {
-                        console.log("**** WAITFOR [" + elXpath + "] WATCH TRIPPED AT interval " + waitInter + " (max:" + maxInter + ")");
-                        callback();
-                    }
-                }, waitTime);
-            };
-            //
-            // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
-            // before we proceed to customize the page...
-            //
-            waitFor( function(){
-                //
-                //  Change the Description
-                //
-                var updatesDescription = document.getElementById("asDesc");
-                var originalText = updatesDescription.textContent;
-                updatesDescription.textContent = "Hello Customizer: " + originalText;
-                updatesDescription.style = "color:#ff0000";
-                //
-                //  Change the Greetings string
-                //  Note that the username of the currently Logged in user can be obtained by means of the
-                //  global variable "lconn.homepage.userName". This variable is defined by the 
-                //  IBM Connections Home page
-                //
-                dojo.query("span.shareSome-title")[0].textContent = "Hello " + lconn.homepage.userName + " -";
-                //
-                // wait until the "loading..." node has been hidden, indicating that we have loaded content.
-                //
-            }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
+                              clearInterval(intId);
+                              if (waitInter >= maxInter) { 
+                                  console.log("**** WAITFOR [" + elXpath + "] WATCH EXPIRED!!! interval " + waitInter + " (max:" + maxInter + ")");
+                              } else {
+                                  console.log("**** WAITFOR [" + elXpath + "] WATCH TRIPPED AT interval " + waitInter + " (max:" + maxInter + ")");
+                                  callback();
+                              }
+                          }, waitTime);
+          };
+          waitFor(function(){
+                    //
+                    //  Change the Greetings string
+                    //  Note that the username of the currently Logged in user can be obtained by means of the
+                    //  global variable "lconn.homepage.userName". This variable is defined by the 
+                    //  IBM Connections Home page
+                    //
+                    dojo.query("span.shareSome-title")[0].textContent = "Hello " + lconn.homepage.userName + " -";
+                    //
+                    // Change the Description
+                    //
+                    var updatesDescription = document.getElementById("asDesc");
+                    var originalText = updatesDescription.textContent;
+                    updatesDescription.textContent = "Hello Customizer: " + originalText;
+                    updatesDescription.style = "color:#ff0000";
+          }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
           alert("Exception occurred in helloWorld: " + e);
       }
